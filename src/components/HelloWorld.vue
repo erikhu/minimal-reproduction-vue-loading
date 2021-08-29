f20d6ae495c6c24f2da4bf973f9f618443f83c95
Erik Gonzalez <erikhuboy@gmail.com>
2021-08-28 21:33:25 -0500

    mininal reproduction

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <Spinner/>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -33,8 +35,13 @@
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import Spinner from './Spinner.vue';
 
-@Component
+@Component({
+    components: {
+        Spinner,
+    }
+})
 export default class HelloWorld extends Vue {
   @Prop() private msg!: string;
 }
---
+++ src/components/Spinner.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <loading :active="true"/>
+    </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+
+@Component({
+    components: {
+        Loading
+    }
+})
+export default class Spinner extends Vue {
+}
+</script>
